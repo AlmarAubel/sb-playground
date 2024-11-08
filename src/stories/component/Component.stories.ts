64064322c38add8453af2b2211bfe63d3e318d94
almaraubel <almar@aubel.nl>
2024-11-08 09:32:04 +0100

    Add failing test

--- src/stories/component/Component.stories.ts
+++ src/stories/component/Component.stories.ts
@@ -8,23 +8,22 @@ function sleep(ms: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-
 const meta = {
   component: MyPage,
   render: args => ({
     components: { MyPage },
     setup: () => {
-      return {args}
+      return { args }
     },
     template: `
-      <my-page v-bind="args" /> `
+      <my-page v-bind="args" /> `,
   }),
   parameters: {
     msw: {
-      handlers: [getPetsMock()]
-    }
-  }
-} satisfies Meta<typeof MyPage >
+      handlers: [getPetsMock()],
+    },
+  },
+} satisfies Meta<typeof MyPage>
 
 export default meta
 type Story = StoryObj<typeof meta>
@@ -36,26 +35,35 @@ export const Case1: Story = {
   args: { id: '2' },
   play: async ({ canvasElement, step }) => {
     const canvas = within(canvasElement)
-    await step('Can find text', async ()=> {
-      const text = await waitFor(() => canvas.getByText(/slurp/i),{ timeout: 10000 })
+    await step('Can find text', async () => {
+      const text = await waitFor(() => canvas.getByText(/slurp/i), {
+        timeout: 10000,
+      })
       await expect(text).toBeVisible()
     })
+  },
+}
 
-  }
+export const Case1WithoutStep: Story = {
+  args: { id: '2' },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    const text = await waitFor(() => canvas.getByText(/slurp/i), {
+      timeout: 10000,
+    })
+    await expect(text).toBeVisible()
+  },
 }
 
 export const Case2: Story = {
   args: { id: '2' },
   play: async ({ canvasElement, step }) => {
     const canvas = within(canvasElement)
-    await step('Can find text', async ()=> {
-      await sleep(5000);
+    await step('Can find text', async () => {
+      await sleep(5000)
       const text = canvas.getByText(/slurp/i)
       await expect(text).toBeVisible()
     })
-
-  }
+  },
 }
-
-
-
